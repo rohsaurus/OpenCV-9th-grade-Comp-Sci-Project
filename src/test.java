48033236016eed2cf06e83d6ca4ff129191e3422
rohsaurus <55811427+rohsaurus@users.noreply.github.com>
2020-03-13 09:06:02 -0400

    Changed dir in code so images input and output correctly

--- src/test.java
+++ src/test.java
@@ -19,10 +19,10 @@ public class test {
     public static void main(String[] args) {
         System.loadLibrary( Core.NATIVE_LIBRARY_NAME ); //load the necessary library for opencv
         for (int i = 1; i <= 374; i++) {
-            Mat source = Imgcodecs.imread("C:\\Users\\mpari\\Documents\\coding projects\\Java\\OpenCV Pictures\\IMG_1383.jpg", Imgcodecs.IMREAD_GRAYSCALE ); //(" + i +")   THIS IS FOR THE FINAL IMAGE NAME when using multiple images loads in the image as grayscale - the i will change with each loop of the program, automatically running for each image
+            Mat source = Imgcodecs.imread("C:\\Users\\mpari\\Documents\\coding projects\\Java\\PiciticanyOpenCVProject\\OpenCV-9th-grade-Comp-Sci-Project\\OpenCV Pictures\\IMG_1383.jpg", Imgcodecs.IMREAD_GRAYSCALE ); //(" + i +")   THIS IS FOR THE FINAL IMAGE NAME when using multiple images loads in the image as grayscale - the i will change with each loop of the program, automatically running for each image
             Mat destination = new Mat(source.rows(), source.cols(), source.type());
             source.convertTo(destination, CvType.CV_8S);
-            Imgcodecs.imwrite("C:\\Users\\mpari\\Documents\\coding projects\\Java\\OpenCV Pictures\\IMG_1383GRAYSCALE.jpg", destination); //write the image as grayscale to a new file
+            Imgcodecs.imwrite("C:\\Users\\mpari\\Documents\\coding projects\\Java\\PiciticanyOpenCVProject\\OpenCV-9th-grade-Comp-Sci-Project\\OpenCV Pictures\\IMG_1383GRAYSCALE.jpg", destination); //write the image as grayscale to a new file
             System.out.println("rows = " + destination.rows()); //print out amount of rows in the image
             System.out.println("columns = " + destination.cols()); //and the amount of columns
             int channels = destination.channels();
@@ -44,13 +44,13 @@ public class test {
             int leftx = largest(intenseArray2) - 531; //to get bottom left corner of where to crop the image, 531 is about how far away from the centerpoint each one is on X and Y (negative and positive)
             int downy = largest(intenseArray) - 531;
             Rect rectCrop = new Rect(leftx, downy, 1050, 1050); //crops the image based on the lower left corner of the image, going 1050 pixels right and 1050 pixels up to generate a square image
-            Mat last = Imgcodecs.imread("C:\\Users\\mpari\\Documents\\coding projects\\Java\\OpenCV Pictures\\IMG_1383.jpg"); //Loads in the original image to crop
+            Mat last = Imgcodecs.imread("C:\\Users\\mpari\\Documents\\coding projects\\Java\\PiciticanyOpenCVProject\\OpenCV-9th-grade-Comp-Sci-Project\\OpenCV Pictures\\IMG_1383.jpg"); //Loads in the original image to crop
             Mat image_output  = last.submat(rectCrop); //applies to image
-            Imgcodecs.imwrite("C:\\Users\\mpari\\Documents\\coding projects\\IMG1383ISTundCROPPED.jpg", image_output); //writes the final cropped image
-            Mat croppedimage = Imgcodecs.imread("C:\\Users\\mpari\\Downloads\\IMG1383ISTundCROPPED.jpg", Imgcodecs.IMREAD_GRAYSCALE );
+            Imgcodecs.imwrite("C:\\Users\\mpari\\Documents\\coding projects\\Java\\PiciticanyOpenCVProject\\OpenCV-9th-grade-Comp-Sci-Project\\OpenCV Pictures\\IMG1383ISTundCROPPED.jpg", image_output); //writes the final cropped image
+            Mat croppedimage = Imgcodecs.imread("C:\\Users\\mpari\\Documents\\coding projects\\Java\\PiciticanyOpenCVProject\\OpenCV-9th-grade-Comp-Sci-Project\\OpenCV Pictures\\IMG1383ISTundCROPPED.jpg", Imgcodecs.IMREAD_GRAYSCALE );
             Mat croppedgrayscale = new Mat(croppedimage.rows(), croppedimage.cols(), croppedimage.type());
             croppedimage.convertTo(destination, CvType.CV_8S);
-            Imgcodecs.imwrite("C:\\Users\\mpari\\Documents\\coding projects\\Java\\OpenCV Pictures\\IMG_1383CROPPEdGRAYSCALE.jpg", croppedgrayscale);
+            Imgcodecs.imwrite("C:\\Users\\mpari\\Documents\\coding projects\\Java\\PiciticanyOpenCVProject\\OpenCV-9th-grade-Comp-Sci-Project\\OpenCV Pictures\\IMG_1383CROPPEdGRAYSCALE.jpg", croppedgrayscale);
         }
     }
 }
